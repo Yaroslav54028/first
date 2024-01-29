146d59f9f4c1f957e201d2589174a110aee22755
florx]p[eom0kiby80 <yastarinsky@yandex.ru>
2024-01-29 15:50:10 +0300

    1

--- app/src/main/java/com/example/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -3,9 +3,13 @@ package com.example.myfirstapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,6 +19,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myfirstapplication.ui.theme.MyFirstApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,9 +29,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun NumberRow(){
-    Row(modifier = Modifire.padding(vertical = 12.dp)) {
-
+fun NumberRow(numberList: List<Int>, isView: Int,  ){
+    Row(modifier = Modifier.padding(vertical = 12.dp)){
+       numberList.forEach{number ->
+           Box(modifier = Modifier
+               .padding(horizontal = 5.dp)
+               .height(100.dp)
+               .width(110.dp)
+           ){
+               if (number)
+           }
+       }
     }
-
 }
